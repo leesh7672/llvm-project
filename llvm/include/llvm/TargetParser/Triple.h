17a9a03156a7b228e6bb8a0fe1682669126f45d3
Seho Lee <leesh7672@gmail.com>
2026-01-20 13:24:41 +0900

    [SPEX64] Add the triple

--- llvm/include/llvm/TargetParser/Triple.h
+++ llvm/include/llvm/TargetParser/Triple.h
@@ -82,6 +82,7 @@ public:
     sparc,       // Sparc: sparc
     sparcv9,     // Sparcv9: Sparcv9
     sparcel,     // Sparc: (endianness = little). NB: 'Sparcle' is a CPU variant
+    spex64, // SPEX64
     systemz,     // SystemZ: s390x
     tce,         // TCE (http://tce.cs.tut.fi/): tce
     tcele,       // TCE little endian (http://tce.cs.tut.fi/): tcele
