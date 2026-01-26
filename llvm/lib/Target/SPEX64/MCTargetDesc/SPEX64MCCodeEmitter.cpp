e975a8b8f69b77d39cedcd5d835dcb354630650d
Seho Lee <leesh7672@gmail.com>
2026-01-27 08:24:39 +0900

    [SPEX] Try to solve problems

--- llvm/lib/Target/SPEX64/MCTargetDesc/SPEX64MCCodeEmitter.cpp
+++ llvm/lib/Target/SPEX64/MCTargetDesc/SPEX64MCCodeEmitter.cpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "SPEX64MCTargetDesc.h"
+#include "SPEX64FixupKinds.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/MC/MCCodeEmitter.h"
 #include "llvm/MC/MCContext.h"
@@ -109,7 +110,9 @@ void SPEX64MCCodeEmitter::encodeInstruction(const MCInst &MI,
       Imm64 = static_cast<uint64_t>(ImmOp->getImm());
       Imm32 = static_cast<uint32_t>(Imm64);
     } else if (ImmOp->isExpr()) {
-      MCFixupKind Kind = (Size == 8) ? FK_Data_4 : FK_Data_8;
+      MCFixupKind Kind = (Size == 8)
+          ? (MCFixupKind)SPEX64::fixup_spex64_32
+          : (MCFixupKind)SPEX64::fixup_spex64_64;
       Fixups.push_back(MCFixup::create(4, ImmOp->getExpr(), Kind));
     }
   }
@@ -131,4 +134,4 @@ MCCodeEmitter *createSPEX64MCCodeEmitter(const MCInstrInfo &MCII,
 
 #include "SPEX64GenMCCodeEmitter.inc"
 
-} // namespace llvm
+} // namespace llvm--- llvm/lib/Target/SPEX64/MCTargetDesc/SPEX64ELFObjectWriter.cpp
