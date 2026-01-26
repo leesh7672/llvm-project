f2c02dda83fe33bef2ba113cc345e9bc8547edd1
Seho Lee <leesh7672@gmail.com>
2026-01-26 23:33:22 +0900

    [SPEX64] Correct an error

--- llvm/lib/BinaryFormat/ELF.cpp
+++ llvm/lib/BinaryFormat/ELF.cpp
@@ -197,6 +197,7 @@ uint16_t ELF::convertArchNameToEMachine(StringRef Arch) {
       .Case("ve", EM_VE)
       .Case("csky", EM_CSKY)
       .Case("loongarch", EM_LOONGARCH)
+      .Case("spex64", EM_SPEX64)
       .Default(EM_NONE);
 }
 
@@ -628,6 +629,8 @@ StringRef ELF::convertEMachineToArchName(uint16_t EMachine) {
     return "csky";
   case EM_LOONGARCH:
     return "loongarch";
+  case EM_SPEX64:
+    return "spex64";
   default:
     return "None";
   }
